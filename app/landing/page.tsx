0ab737a7daa9e1f257f59685a6b374c2174a5fa1
balatharunr <r.balatharun@gmail.com>
2026-05-03 22:17:27 +0530

    refactor: redirect logic from landing page

--- app/landing/page.tsx
+++ app/landing/page.tsx
@@ -1,12 +1,5 @@
-import { redirect } from "next/navigation";
 import BrochifyLanding from "@/components/landing/BrochifyLanding";
-import { getServerSession, homeRouteForRole } from "@/lib/server/auth";
 
 export default async function LandingPage() {
-  const session = await getServerSession();
-  if (session) {
-    redirect(homeRouteForRole(session.role));
-  }
-
   return <BrochifyLanding />;
 }
